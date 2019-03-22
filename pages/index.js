b0403c32286770bb5a0cf35d3ca8d34ece738982
Priyanka Kundra <priyanka.kundra@gmail.com>
2019-03-22 16:18:50 +0100

    Change title

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Layout from "../layouts/default";
 
 export default () => (
   <Layout>
-    <h1> Welcome to my Website</h1>
+    <h1> Welcome to my Websitez</h1>
     <p> read all out xyz and get all the insights</p>
     <h2>Hello again</h2>
     <p>dewde f df sd s</p>
