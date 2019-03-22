9eaa958207ecf548d4686ce74a0d8d09bca17271
priyankakundra <48826028+priyankakundra@users.noreply.github.com>
2019-03-22 16:03:31 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Layout from "../layouts/default";
+
+export default () => (
+  <Layout>
+    <h1> Welcome to my Website</h1>
+    <p> read all out xyz and get all the insights</p>
+    <h2>Hello again</h2>
+    <p>dewde f df sd s</p>
+    <h3> bye bye </h3>
+    <p>
+      Yet another paragraph. Money performance expenses federal maturities
+      investment grade bonds credit quality. Volatile corporate interest rate
+      appeal.
+    </p>
+  </Layout>
+);
